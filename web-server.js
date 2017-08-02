2323a977198ed2d6e3c1199fe1575b695815e167
Sedrior <30593425+Sedrior@users.noreply.github.com>
2017-08-02 12:11:33 +0300

    commit

--- web-server.js
+++ web-server.js
@@ -9,6 +9,6 @@ var rootPath=path.normalize(__dirname + '/../');
 
 app.use(express.static(rootPath + '/app'));
 
-app.listen(8000);
+app.listen(3000);
 
-console.log("Listening on port 8000...")
+console.log("Listening on port 3000...")
