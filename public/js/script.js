a1c34815579e0f642bb87782f907c029cdb2ba49
Sedrior <30593425+Sedrior@users.noreply.github.com>
2017-08-11 12:18:03 +0300

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -451,6 +451,8 @@ scotchApp.controller('MyBookingsController',
     function($scope, $http, $cookieStore) {
         if($cookieStore.get('key')) {
             $scope.Bookings = [];
+            $scope.type=[];
+            var i,j;
             $scope.getBookings = function () {
                 var Key = $cookieStore.get('key');
                 $http.defaults.headers.common['Authorization'] = 'Basic ' + Key;
@@ -459,6 +461,25 @@ scotchApp.controller('MyBookingsController',
                     function (obj) {
 
                         $scope.Bookings = obj.data.records;
+                        console.dir($scope.Bookings);
+                        var url2 = 'https://api-test.insoftd.com/v1/client/car_type';
+                        $http.get(url2).then(
+                          function(obj2){
+
+                              $scope.type=obj2.data.records;
+                              console.dir($scope.type);
+                              for(i=0;i<$scope.Bookings.length;i++) {
+                                  for (j = 0; j < $scope.type.length; j++) {
+                                      if ($scope.Bookings[i].id_car_type == $scope.type[j].id_type) {
+                                          $scope.Bookings[i].id_car_type = $scope.type[j].type;
+                                      }
+                                  }
+                              }
+                          },
+                            function(){
+                              console.dir('error');
+                            }
+                        );
 
                     },
                     function () {
